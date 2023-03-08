26205a83b048dbf3e23f0d438388349190079a05
Choirool <khoiroelf@gmail.com>
2023-03-08 11:45:04 +0800

    init

---
+++ app/components/App.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+    <TabView androidTabsPosition="bottom">
+        <TabViewItem title="Home">
+            <Frame id="items">
+                <Items/>
+            </Frame>
+        </TabViewItem>
+
+        <TabViewItem title="Browse">
+            <Frame id="browse">
+                <Browse/>
+            </Frame>
+        </TabViewItem>
+
+        <TabViewItem title="Search">
+            <Frame id="search">
+                <Search/>
+            </Frame>
+        </TabViewItem>
+    </TabView>
+</template>
+
+<script>
+  import Items from "./Items.vue";
+  import Browse from "./Browse.vue";
+  import Search from "./Search.vue";
+
+  export default {
+    components: {
+      Items,
+      Browse,
+      Search
+    }
+  };
+</script>
+
+<style lang="scss">
+    // Start custom common variables
+    @import "@nativescript/theme/scss/variables/blue";
+    // End custom common variables
+
+    // Custom styles
+
+</style>
